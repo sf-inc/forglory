68d0803fa7f5d2173da52264b9d3a49b9cfc8a28
G G <gael.garcia@etu.univ-lyon.fr>
2020-09-29 19:40:33 +0200

    Init

---
+++ src/main/java/com/github/galatynf/forglory/Forglory.java
@@ -0,0 +1,10 @@
+package com.github.galatynf.forglory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Forglory implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
