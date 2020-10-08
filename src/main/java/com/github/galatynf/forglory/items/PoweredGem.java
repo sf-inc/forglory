0aa34027edd1e3896641668fc8afd5e8252c80a3
Charly <charly5bollinger@gmail.com>
2020-10-08 22:11:52 +0200

    Abstract effective PoweredGem
    Set the PoweredGem abstract (oops I had forgot!) and made it effective to use thanks to the mew duck interface that allows to add/change the feats outside of the mixin

--- src/main/java/com/github/galatynf/forglory/items/PoweredGem.java
+++ src/main/java/com/github/galatynf/forglory/items/PoweredGem.java
@@ -1,9 +1,24 @@
 package com.github.galatynf.forglory.items;
 
+import com.github.galatynf.forglory.enumFeat.Feats;
+import com.github.galatynf.forglory.imixin.IFeatsMixin;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
 
-public class PoweredGem extends Item {
+public abstract class PoweredGem extends Item {
+    protected Feats feat;
     protected PoweredGem(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ((IFeatsMixin)user).addOrUpdateFeat(feat);
+        return new TypedActionResult<>(ActionResult.SUCCESS, user.getStackInHand(hand));
     }
 }
