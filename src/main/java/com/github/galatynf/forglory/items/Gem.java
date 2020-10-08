4f39de5a16ff2c0134eba800032695f244f4dfd9
Charly <charly5bollinger@gmail.com>
2020-10-08 11:35:21 +0200

    Definition of abstract superclasses
    One for the tier0 gems which are used to craft the first tier.
    One for every other tiers which gives you abilities.

---
+++ src/main/java/com/github/galatynf/forglory/items/Gem.java
@@ -0,0 +1,9 @@
+package com.github.galatynf.forglory.items;
+
+import net.minecraft.item.Item;
+
+public abstract class Gem extends Item {
+    protected Gem(Settings settings) {
+        super(settings);
+    }
+}
---
+++ src/main/java/com/github/galatynf/forglory/items/PoweredGem.java
@@ -0,0 +1,9 @@
+package com.github.galatynf.forglory.items;
+
+import net.minecraft.item.Item;
+
+public class PoweredGem extends Item {
+    protected PoweredGem(Settings settings) {
+        super(settings);
+    }
+}
