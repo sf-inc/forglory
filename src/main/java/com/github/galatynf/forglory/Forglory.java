760204e1924c51b994f3a735e83c0cfa6d513147
Charly <charly5bollinger@gmail.com>
2020-10-08 11:56:13 +0200

    Add damage gem tier 0
    No ability, just a crafting element

--- src/main/java/com/github/galatynf/forglory/Forglory.java
+++ src/main/java/com/github/galatynf/forglory/Forglory.java
@@ -1,10 +1,25 @@
 package com.github.galatynf.forglory;
 
+import com.github.galatynf.forglory.items.tier0.DamageGem;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Forglory implements ModInitializer {
+    public static final ItemGroup forGlory = FabricItemGroupBuilder.create(
+            new Identifier("forglory", "gems"))
+            .icon(() -> new ItemStack(Items.QUARTZ))
+            .build();
+
+    public static final DamageGem damageGem = new DamageGem(new Item.Settings().group(forGlory));
+
     @Override
     public void onInitialize() {
-
+        Registry.register(Registry.ITEM, new Identifier("forglory", "damage_gem"), damageGem);
     }
 }
---
+++ src/main/java/com/github/galatynf/forglory/items/tier0/DamageGem.java
@@ -0,0 +1,9 @@
+package com.github.galatynf.forglory.items.tier0;
+
+import com.github.galatynf.forglory.items.Gem;
+
+public class DamageGem extends Gem {
+    public DamageGem(Settings settings) {
+        super(settings);
+    }
+}
