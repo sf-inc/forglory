3b4d5d08208a0df2ac9de7b19b5b3ce200060afe
Charly <charly5bollinger@gmail.com>
2020-10-08 22:09:43 +0200

    Added possibility to add a feat
    Now we can add (or change) a feat to a player, and so: outside the mixin! Thanks to duck interface

---
+++ src/main/java/com/github/galatynf/forglory/imixin/IFeatsMixin.java
@@ -0,0 +1,7 @@
+package com.github.galatynf.forglory.imixin;
+
+import com.github.galatynf.forglory.enumFeat.Feats;
+
+public interface IFeatsMixin {
+    void addOrUpdateFeat(Feats feat);
+}
--- src/main/java/com/github/galatynf/forglory/mixin/FeatsMixin.java
+++ src/main/java/com/github/galatynf/forglory/mixin/FeatsMixin.java
@@ -2,13 +2,19 @@ package com.github.galatynf.forglory.mixin;
 
 import com.github.galatynf.forglory.enumFeat.Feats;
 import com.github.galatynf.forglory.enumFeat.Tier;
+import com.github.galatynf.forglory.imixin.IFeatsMixin;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import java.util.HashMap;
 
 @Mixin(PlayerEntity.class)
-public class FeatsMixin {
+public class FeatsMixin implements IFeatsMixin {
     @Unique
-    protected HashMap<Tier, Feats> feats = new HashMap<>();
+    private HashMap<Tier, Feats> feats = new HashMap<>();
+
+    @Override
+    public void addOrUpdateFeat(final Feats feat) {
+        feats.put(feat.tier, feat);
+    }
 }
