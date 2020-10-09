9015f5923b4a103daefebc9668790d654c80489c
Charly <charly5bollinger@gmail.com>
2020-10-09 17:37:25 +0200

    Speed feat implemented

---
+++ src/main/java/com/github/galatynf/forglory/mixin/mobility/SpeedMixin.java
@@ -0,0 +1,35 @@
+package com.github.galatynf.forglory.mixin.mobility;
+
+import com.github.galatynf.forglory.enumFeat.Feats;
+import com.github.galatynf.forglory.enumFeat.Tier;
+import com.github.galatynf.forglory.imixin.IAdrenalinMixin;
+import com.github.galatynf.forglory.imixin.IFeatsMixin;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntity.class)
+public abstract class SpeedMixin extends LivingEntity {
+    protected SpeedMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(at=@At("INVOKE"), method = "tick")
+    void addSpeedEffect(CallbackInfo ci) {
+        Feats feat = ((IFeatsMixin)this).getFeat(Tier.TIER1);
+        if (feat == null) return;
+        if (feat.equals(Feats.SPEED)) {
+            if (((IAdrenalinMixin)this).getAdrenalin() > Tier.TIER1.threshold) {
+                this.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 1, 0));
+            }
+        }
+    }
+}
