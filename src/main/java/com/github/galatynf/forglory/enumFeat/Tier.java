2eb10b610e3b843175381c30dce7941b2ba27aa7
Charly <charly5bollinger@gmail.com>
2020-10-09 16:38:55 +0200

    fixup rename to tier

--- src/main/java/com/github/galatynf/forglory/enumFeat/Tier.java
+++ src/main/java/com/github/galatynf/forglory/enumFeat/Tier.java
@@ -1,8 +1,13 @@
 package com.github.galatynf.forglory.enumFeat;
 
 public enum Tier {
-    TIER1,
-    TIER2,
-    TIER3,
-    TIER4
+    TIER1(2000),
+    TIER2(4000),
+    TIER3(6000),
+    TIER4(8000);
+
+    public double threshold;
+    Tier(double threshold) {
+        this.threshold = threshold;
+    }
 }
