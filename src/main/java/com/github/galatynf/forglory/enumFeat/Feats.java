ec1f8d152ad310195f51cb665b51a6d51bd16361
Charly <charly5bollinger@gmail.com>
2020-10-20 10:54:45 +0200

    Added feats cooldown declaration
    Cooldown not implemented yet but now feats are associated with a specified cooldown

--- src/main/java/com/github/galatynf/forglory/enumFeat/Feats.java
+++ src/main/java/com/github/galatynf/forglory/enumFeat/Feats.java
@@ -1,34 +1,35 @@
 package com.github.galatynf.forglory.enumFeat;
 
-// Class only meant to regroup enums. Should not be modified in any means
 public enum Feats {
-    DOG(Tier.TIER1),
-    FIRE_TRAIL(Tier.TIER1),
-    HEALING_FIST(Tier.TIER1),
-    NO_HUNGER(Tier.TIER1),
-    SMITE(Tier.TIER1),
-    SPEED(Tier.TIER1),
+    DOG(Tier.TIER1, 0),
+    FIRE_TRAIL(Tier.TIER1, 0),
+    HEALING_FIST(Tier.TIER1, 0), // Add cooldown
+    NO_HUNGER(Tier.TIER1, 0),
+    SMITE(Tier.TIER1, 0),
+    SPEED(Tier.TIER1, 0),
 
-    DASH(Tier.TIER2),
-    FIRE_RESISTANCE(Tier.TIER2),
-    KNOCKBACK_FIST(Tier.TIER2),
-    MOUNTAIN(Tier.TIER2),
-    STRENGTH(Tier.TIER2),
+    DASH(Tier.TIER2, 0), // Add cooldown
+    FIRE_RESISTANCE(Tier.TIER2, 0),
+    KNOCKBACK_FIST(Tier.TIER2, 0), // Add cooldown
+    MOUNTAIN(Tier.TIER2, 0), // Add cooldown
+    STRENGTH(Tier.TIER2, 0),
 
-    COMPANION_HEAL(Tier.TIER3),
-    DAMAGE_SLOWED(Tier.TIER3),
-    FIRE_ZONE(Tier.TIER3),
-    JUMP_BOOST(Tier.TIER3),
-    SHIELD(Tier.TIER3),
+    COMPANION_HEAL(Tier.TIER3, 0),
+    DAMAGE_SLOWED(Tier.TIER3, 0),
+    FIRE_ZONE(Tier.TIER3, 0),
+    JUMP_BOOST(Tier.TIER3, 0),
+    SHIELD(Tier.TIER3, 0),
 
-    BEES(Tier.TIER4),
-    INVISIBLE(Tier.TIER4),
-    FIREWORKER(Tier.TIER4),
-    INSTANT_KILL(Tier.TIER4),
-    LAST_STAND(Tier.TIER4);
+    BEES(Tier.TIER4, 0),
+    INVISIBLE(Tier.TIER4, 0),
+    FIREWORKER(Tier.TIER4, 0), // Add cooldown
+    INSTANT_KILL(Tier.TIER4, 0), // Add cooldown
+    LAST_STAND(Tier.TIER4, 0);
 
     public Tier tier;
-    Feats(Tier tier) {
+    public Integer cooldown;
+    Feats(final Tier tier, final Integer cooldown) {
         this.tier = tier;
+        this.cooldown = cooldown;
     }
 }
