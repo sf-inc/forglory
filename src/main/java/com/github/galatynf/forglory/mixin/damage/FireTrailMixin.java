4cc34fa6ff9b9766481ca4df09e4b1e669f1b07f
Charly <charly5bollinger@gmail.com>
2020-10-22 20:59:48 +0200

    Update fire from trail
    Now the fire trail spawns quick fire (closer cause it doesn't damage)

--- src/main/java/com/github/galatynf/forglory/mixin/damage/FireTrailMixin.java
+++ src/main/java/com/github/galatynf/forglory/mixin/damage/FireTrailMixin.java
@@ -1,17 +1,15 @@
 package com.github.galatynf.forglory.mixin.damage;
 
+import com.github.galatynf.forglory.Forglory;
 import com.github.galatynf.forglory.enumFeat.Feats;
 import com.github.galatynf.forglory.enumFeat.Tier;
 import com.github.galatynf.forglory.imixin.IAdrenalinMixin;
 import com.github.galatynf.forglory.imixin.IFeatsMixin;
-import net.minecraft.block.AbstractFireBlock;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -30,12 +28,7 @@ public abstract class FireTrailMixin extends Entity {
         if (feat.equals(Feats.FIRE_TRAIL)) {
             if (((IAdrenalinMixin)this).getAdrenalin() > Tier.TIER1.threshold) {
                 BlockPos blockPos = this.getBlockPos().offset(this.getMovementDirection().getOpposite());
-                blockPos = blockPos.offset(this.getMovementDirection().getOpposite());
-                WorldAccess worldAccess = this.world;
-                if (AbstractFireBlock.method_30032(worldAccess, blockPos)) {
-                    BlockState blockState = AbstractFireBlock.getState(worldAccess, blockPos);
-                    worldAccess.setBlockState(blockPos, blockState, 11);
-                }
+                this.world.setBlockState(blockPos, Forglory.quickFireBlock.getDefaultState());
             }
         }
     }
