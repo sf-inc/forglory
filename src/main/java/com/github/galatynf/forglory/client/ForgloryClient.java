5e1278352f86680526d73a8221d9c298b9bef0e7
Charly <charly5bollinger@gmail.com>
2020-12-28 18:55:52 +0100

    Added a keybind to activate feat
    For now it doesn't do anything: we just added the key in the menu

--- src/main/java/com/github/galatynf/forglory/client/ForgloryClient.java
+++ src/main/java/com/github/galatynf/forglory/client/ForgloryClient.java
@@ -1,19 +1,28 @@
 package com.github.galatynf.forglory.client;
 
 import com.github.galatynf.forglory.Forglory;
-import com.github.galatynf.forglory.blocks.QuickFireBlock;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.render.RenderLayer;
-
-import javax.print.attribute.standard.Fidelity;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class ForgloryClient implements ClientModInitializer {
+    private static KeyBinding activateFeatKey;
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(Forglory.quickFireBlock, RenderLayer.getCutout());
+
+        activateFeatKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.forglory.activateFeatKey",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_Z,
+                "category.forglory.keys"
+        ));
     }
 }
