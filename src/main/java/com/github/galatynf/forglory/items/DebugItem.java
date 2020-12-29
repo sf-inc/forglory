76bc9a7241c6a4a6b3ea1e6085bddef6a299cafd
Charly <charly5bollinger@gmail.com>
2020-12-29 21:21:08 +0100

    Improve debug item
    Now it really depends on the current adrenalin and change amount depending on the current threshold

--- src/main/java/com/github/galatynf/forglory/items/DebugItem.java
+++ src/main/java/com/github/galatynf/forglory/items/DebugItem.java
@@ -1,6 +1,5 @@
 package com.github.galatynf.forglory.items;
 
-import com.github.galatynf.forglory.enumFeat.Tier;
 import com.github.galatynf.forglory.imixin.IAdrenalinMixin;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -10,6 +9,8 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
+import static com.github.galatynf.forglory.config.constants.AdrenalinConfig.*;
+
 public class DebugItem extends Item {
     public DebugItem(Settings settings) {
         super(settings);
@@ -18,11 +19,44 @@ public class DebugItem extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if (!world.isClient) {
-            if (user.isSneaking())
-                ((IAdrenalinMixin)user).addAdrenalin(-Tier.TIER1.threshold);
-            else
-                ((IAdrenalinMixin)user).addAdrenalin(Tier.TIER1.threshold);
+            float adrenalin = ((IAdrenalinMixin)user).getAdrenalin();
+            float toAdd = amountToAdd(adrenalin, user.isSneaking());
+            ((IAdrenalinMixin)user).addAdrenalin(toAdd);
         }
         return new TypedActionResult<>(ActionResult.SUCCESS, user.getStackInHand(hand));
+    }
+
+    private float amountToAdd(final float adrenalin, final boolean toLoose) {
+        float amount;
+        if (adrenalin < TIER1_THRESHOLD) {
+            if (toLoose) {
+                amount = -adrenalin;
+            } else {
+                amount = (TIER2_THRESHOLD - TIER1_THRESHOLD) / 2.f + TIER1_THRESHOLD;
+                amount -= adrenalin;
+            }
+        } else if (adrenalin < TIER2_THRESHOLD) {
+            if (toLoose) {
+                amount = TIER1_THRESHOLD - adrenalin;
+            } else {
+                amount = (TIER3_THRESHOLD - TIER2_THRESHOLD) / 2.f + TIER2_THRESHOLD;
+                amount -= adrenalin;
+            }
+        } else if (adrenalin < TIER3_THRESHOLD) {
+            if (toLoose) {
+                amount = TIER2_THRESHOLD - adrenalin;
+            } else {
+                amount = (TIER4_THRESHOLD - TIER3_THRESHOLD) / 2.f + TIER3_THRESHOLD;
+                amount -= adrenalin;
+            }
+        } else {
+            if (toLoose) {
+                amount = TIER3_THRESHOLD - adrenalin;
+            } else {
+                amount = (MAX_AMOUNT - TIER4_THRESHOLD) / 2.f + TIER4_THRESHOLD;
+                amount -= adrenalin;
+            }
+        }
+        return amount;
     }
 }
