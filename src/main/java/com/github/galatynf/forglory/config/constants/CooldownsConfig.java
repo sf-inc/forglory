a3c08506c5b0221009f0b56a17ac49d338be17c7
Charly <charly5bollinger@gmail.com>
2020-12-29 18:41:39 +0100

    Moved cooldowns constants into a file
    For a code without magic numbers: upcoming commit about it again. For feats that have cooldown, and also specify the value for feats with no or a unique cooldown

---
+++ src/main/java/com/github/galatynf/forglory/config/constants/CooldownsConfig.java
@@ -0,0 +1,15 @@
+package com.github.galatynf.forglory.config.constants;
+
+public class CooldownsConfig {
+    private CooldownsConfig() {}
+
+    public static final Integer UNIQUE_COOLDOWN = -1;
+    public static final Integer NO_COOLDOWN = 0;
+
+    public static Integer HEALING_FIST_COOLDOWN = 0; // TODO Add cooldown
+    public static Integer DASH_COOLDOWN = 0; // TODO Add cooldown
+    public static Integer KNOCKBACK_FIST_COOLDOWN = 150;
+    public static Integer MOUNTAIN_COOLDOWN = 300;
+    public static Integer FIREWORKER_COOLDOWN = 200;
+    public static Integer INSTANT_KILL_COOLDOWN = 0; // TODO Add cooldown
+}
--- src/main/java/com/github/galatynf/forglory/enumFeat/Feats.java
+++ src/main/java/com/github/galatynf/forglory/enumFeat/Feats.java
@@ -1,30 +1,32 @@
 package com.github.galatynf.forglory.enumFeat;
 
+import com.github.galatynf.forglory.config.constants.CooldownsConfig;
+
 public enum Feats {
-    DOG(Tier.TIER1, -1),
-    FIRE_TRAIL(Tier.TIER1, 0),
-    HEALING_FIST(Tier.TIER1, 0), // Add cooldown
-    NO_HUNGER(Tier.TIER1, 0),
-    SMITE(Tier.TIER1, 0),
-    SPEED(Tier.TIER1, 0),
+    DOG(Tier.TIER1, CooldownsConfig.UNIQUE_COOLDOWN),
+    FIRE_TRAIL(Tier.TIER1, CooldownsConfig.NO_COOLDOWN),
+    HEALING_FIST(Tier.TIER1, CooldownsConfig.HEALING_FIST_COOLDOWN),
+    NO_HUNGER(Tier.TIER1, CooldownsConfig.NO_COOLDOWN),
+    SMITE(Tier.TIER1, CooldownsConfig.NO_COOLDOWN),
+    SPEED(Tier.TIER1, CooldownsConfig.NO_COOLDOWN),
 
-    DASH(Tier.TIER2, 0), // Add cooldown
-    FIRE_RESISTANCE(Tier.TIER2, 0),
-    KNOCKBACK_FIST(Tier.TIER2, 100), // Add cooldown
-    MOUNTAIN(Tier.TIER2, 0), // Add cooldown
-    STRENGTH(Tier.TIER2, 0),
+    DASH(Tier.TIER2, CooldownsConfig.DASH_COOLDOWN),
+    FIRE_RESISTANCE(Tier.TIER2, CooldownsConfig.NO_COOLDOWN),
+    KNOCKBACK_FIST(Tier.TIER2, CooldownsConfig.KNOCKBACK_FIST_COOLDOWN),
+    MOUNTAIN(Tier.TIER2, CooldownsConfig.MOUNTAIN_COOLDOWN),
+    STRENGTH(Tier.TIER2, CooldownsConfig.NO_COOLDOWN),
 
-    COMPANION_HEAL(Tier.TIER3, 0),
-    DAMAGE_SLOWED(Tier.TIER3, 0),
-    FIRE_ZONE(Tier.TIER3, 0),
-    JUMP_BOOST(Tier.TIER3, 0),
-    SHIELD(Tier.TIER3, 0),
+    COMPANION_HEAL(Tier.TIER3, CooldownsConfig.NO_COOLDOWN),
+    DAMAGE_SLOWED(Tier.TIER3, CooldownsConfig.NO_COOLDOWN),
+    FIRE_ZONE(Tier.TIER3, CooldownsConfig.NO_COOLDOWN),
+    JUMP_BOOST(Tier.TIER3, CooldownsConfig.NO_COOLDOWN),
+    SHIELD(Tier.TIER3, CooldownsConfig.NO_COOLDOWN),
 
-    BEES(Tier.TIER4, 0),
-    INVISIBLE(Tier.TIER4, 0),
-    FIREWORKER(Tier.TIER4, 400), // Add cooldown
-    INSTANT_KILL(Tier.TIER4, 0), // Add cooldown
-    LAST_STAND(Tier.TIER4, 0);
+    BEES(Tier.TIER4, CooldownsConfig.NO_COOLDOWN),
+    INVISIBLE(Tier.TIER4, CooldownsConfig.NO_COOLDOWN),
+    FIREWORKER(Tier.TIER4, CooldownsConfig.FIREWORKER_COOLDOWN),
+    INSTANT_KILL(Tier.TIER4, CooldownsConfig.INSTANT_KILL_COOLDOWN),
+    LAST_STAND(Tier.TIER4, CooldownsConfig.NO_COOLDOWN);
 
     public Tier tier;
     public Integer cooldown;
