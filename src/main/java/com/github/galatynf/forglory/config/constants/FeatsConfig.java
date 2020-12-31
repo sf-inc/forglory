d8a82c8780a958c7cd2ac0d0fbb9280d3904987b
Galatynf <galvana.ciaran@outlook.fr>
2020-12-31 14:35:35 +0100

    Added configuration to heal trail

--- src/main/java/com/github/galatynf/forglory/config/constants/FeatsConfig.java
+++ src/main/java/com/github/galatynf/forglory/config/constants/FeatsConfig.java
@@ -6,4 +6,7 @@ public class FeatsConfig {
     public static float SMITE_MULTIPLIER = 1.5f;
     public static int MOUNTAIN_HEIGHT = 4;
     public static int LIFE_STEAL_MAX_AMOUNT = 3;
+    public static float HEAL_TRAIL_RADIUS = 2.0F;
+    public static int HEAL_TRAIL_FREQUENCY = 20;
+    public static int HEAL_TRAIL_WAIT_TIME = 10;
 }
--- src/main/java/com/github/galatynf/forglory/mixin/heal/HealTrailMixin.java
+++ src/main/java/com/github/galatynf/forglory/mixin/heal/HealTrailMixin.java
@@ -1,29 +1,23 @@
 package com.github.galatynf.forglory.mixin.heal;
 
+import com.github.galatynf.forglory.config.constants.FeatsConfig;
 import com.github.galatynf.forglory.enumFeat.Feats;
 import com.github.galatynf.forglory.enumFeat.Tier;
 import com.github.galatynf.forglory.imixin.IAdrenalinMixin;
 import com.github.galatynf.forglory.imixin.IFeatsMixin;
 import net.minecraft.entity.AreaEffectCloudEntity;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.particle.ItemStackParticleEffect;
-import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.potion.PotionUtil;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Iterator;
 
 @Mixin(PlayerEntity.class)
 public abstract class HealTrailMixin extends LivingEntity {
@@ -40,16 +34,15 @@ public abstract class HealTrailMixin extends LivingEntity {
         if (feat == null) return;
         if (feat.equals(Feats.HEAL_TRAIL)) {
             if (((IAdrenalinMixin) this).getAdrenalin() > Tier.TIER4.threshold) {
-                if (forglory_lastSpawned >= 20 && this.isOnGround()) {
+                if (forglory_lastSpawned >= FeatsConfig.HEAL_TRAIL_FREQUENCY && this.isOnGround()) {
                     AreaEffectCloudEntity areaEffectCloudEntity = new AreaEffectCloudEntity(this.world, this.getX(), this.getY(), this.getZ());
                     areaEffectCloudEntity.setOwner(this);
-                    areaEffectCloudEntity.setRadius(2.0F);
+                    areaEffectCloudEntity.setRadius(FeatsConfig.HEAL_TRAIL_RADIUS);
                     areaEffectCloudEntity.setRadiusOnUse(-0.5F);
-                    areaEffectCloudEntity.setWaitTime(10);
+                    areaEffectCloudEntity.setWaitTime(FeatsConfig.HEAL_TRAIL_WAIT_TIME);
                     areaEffectCloudEntity.setRadiusGrowth(-areaEffectCloudEntity.getRadius() / (float) areaEffectCloudEntity.getDuration());
                     areaEffectCloudEntity.setDuration(10);
                     areaEffectCloudEntity.setParticleType(ParticleTypes.HAPPY_VILLAGER);
-                    //areaEffectCloudEntity.setPotion(potion);
 
                     areaEffectCloudEntity.addEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 40, 0));
 
