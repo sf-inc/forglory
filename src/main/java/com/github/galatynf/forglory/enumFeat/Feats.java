b75b0d039faed80750b1ee1ffd906c3a6f17b8e4
Galatynf <galvana.ciaran@outlook.fr>
2021-01-27 18:17:41 +0100

    Corrected tiers and reordered feats

--- src/main/java/com/github/galatynf/forglory/enumFeat/Feats.java
+++ src/main/java/com/github/galatynf/forglory/enumFeat/Feats.java
@@ -4,35 +4,35 @@ import com.github.galatynf.forglory.config.ModConfig;
 import com.github.galatynf.forglory.config.ConstantsConfig;
 
 public enum Feats {
+    BLOODLUST(Tier.TIER1),
     DOG(Tier.TIER1),
-    FIRE_TRAIL(Tier.TIER1),
-    HEALING_FIST(Tier.TIER1),
     NO_HUNGER(Tier.TIER1),
+    RESISTANCE(Tier.TIER1),
     SMITE(Tier.TIER1),
     SPEED(Tier.TIER1),
-    RESISTANCE(Tier.TIER1),
-    BLOODLUST(Tier.TIER1),
+    STRENGTH(Tier.TIER1),
 
     DASH(Tier.TIER2),
+    FIRE_TRAIL(Tier.TIER2),
+    HEALING_FIST(Tier.TIER2),
     KNOCKBACK_FIST(Tier.TIER2),
     MACHINE_GUN(Tier.TIER2),
     MOUNTAIN(Tier.TIER2),
-    STRENGTH(Tier.TIER2),
 
     COMPANION_HEAL(Tier.TIER3),
     DAMAGE_SLOWED(Tier.TIER3),
-    FIRE_ZONE(Tier.TIER3),
     JUMP_BOOST(Tier.TIER3),
     SHIELD(Tier.TIER3),
     SHIELD_RESISTANCE(Tier.TIER3),
     STRIDERS_GRACE(Tier.TIER3),
 
     BEES(Tier.TIER4),
-    INVISIBLE(Tier.TIER4),
     FIREWORKER(Tier.TIER4),
+    FIRE_ZONE(Tier.TIER4),
+    HEAL_TRAIL(Tier.TIER4),
     INSTANT_KILL(Tier.TIER4),
-    LAST_STAND(Tier.TIER4),
-    HEAL_TRAIL(Tier.TIER4);
+    INVISIBLE(Tier.TIER4),
+    LAST_STAND(Tier.TIER4);
 
     public Tier tier;
     public int cooldown;
