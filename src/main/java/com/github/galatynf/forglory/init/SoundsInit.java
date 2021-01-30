688b88e02b2df76304881623622b843dcce14b1b
Galatynf <galvana.ciaran@outlook.fr>
2021-01-30 21:56:51 +0100

    Added sounds for heroes

--- src/main/java/com/github/galatynf/forglory/init/SoundsInit.java
+++ src/main/java/com/github/galatynf/forglory/init/SoundsInit.java
@@ -19,10 +19,42 @@ public class SoundsInit {
     public static final Identifier TIER_4_EFFECT_ID = new Identifier("forglory:tier_4_overcharged");
     public static SoundEvent tier_4_overcharged_event = new SoundEvent(TIER_4_EFFECT_ID);
 
+
+    public static final Identifier MALE_GRUNT_ID = new Identifier("forglory:male_grunt");
+    public static SoundEvent male_grunt = new SoundEvent((MALE_GRUNT_ID));
+
+    public static final Identifier FEMALE_GRUNT_ID = new Identifier("forglory:female_grunt");
+    public static SoundEvent female_grunt = new SoundEvent((FEMALE_GRUNT_ID));
+
+    public static final Identifier FEMALE_DEATH_ID = new Identifier("forglory:female_death");
+    public static SoundEvent female_death = new SoundEvent((FEMALE_DEATH_ID));
+
+    public static final Identifier MALE_DEATH_ID = new Identifier("forglory:male_death");
+    public static SoundEvent male_death = new SoundEvent((MALE_DEATH_ID));
+
+
+    public static final Identifier INCRE_ID = new Identifier("forglory:incre");
+    public static SoundEvent incre = new SoundEvent((INCRE_ID));
+
+    public static final Identifier DIBILIS_ID = new Identifier("forglory:dibilis");
+    public static SoundEvent dibilis = new SoundEvent((DIBILIS_ID));
+
     public static void init () {
         Registry.register(Registry.SOUND_EVENT, TIER_1_EFFECT_ID, tier_1_whoosh_event);
         Registry.register(Registry.SOUND_EVENT, TIER_2_EFFECT_ID, tier_2_bass_event);
         Registry.register(Registry.SOUND_EVENT, TIER_3_EFFECT_ID, tier_3_strong_bass_event);
         Registry.register(Registry.SOUND_EVENT, TIER_4_EFFECT_ID, tier_4_overcharged_event);
+
+        Registry.register(Registry.SOUND_EVENT, MALE_GRUNT_ID, male_grunt);
+
+        Registry.register(Registry.SOUND_EVENT, FEMALE_GRUNT_ID, female_grunt);
+
+        Registry.register(Registry.SOUND_EVENT, FEMALE_DEATH_ID, female_death);
+
+        Registry.register(Registry.SOUND_EVENT, MALE_DEATH_ID, male_death);
+
+        Registry.register(Registry.SOUND_EVENT, INCRE_ID, incre);
+        Registry.register(Registry.SOUND_EVENT, DIBILIS_ID, dibilis);
+
     }
 }
