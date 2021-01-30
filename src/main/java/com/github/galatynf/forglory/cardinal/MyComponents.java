4e9c92b02caca35869e207f078e8e7335e01e223
Charly <charly5bollinger@gmail.com>
2021-01-30 21:22:34 +0100

    Cardinal component for bee
    Now use the cardinal component api instead of interface mixin so that it keeps the data when deco/reco. Use the same than undead army. Remove old unused interface

--- src/main/java/com/github/galatynf/forglory/cardinal/MyComponents.java
+++ src/main/java/com/github/galatynf/forglory/cardinal/MyComponents.java
@@ -6,6 +6,7 @@ import dev.onyxstudios.cca.api.v3.component.ComponentRegistryV3;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
 import nerdhub.cardinal.components.api.util.RespawnCopyStrategy;
+import net.minecraft.entity.passive.BeeEntity;
 import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.util.Identifier;
 
@@ -21,6 +22,7 @@ public class MyComponents implements EntityComponentInitializer {
     public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
         registry.registerForPlayers(FEATS, player -> new PlayerFeatsComponent(), RespawnCopyStrategy.ALWAYS_COPY);
         registry.registerFor(HeroEntity.class, SUMMONED, hero -> new SummonerComponent());
+        registry.registerFor(BeeEntity.class, SUMMONED, bee -> new SummonerComponent());
         registry.registerFor(WolfEntity.class, SUMMONED, wolf -> new SummonerComponent());
     }
 }
--- src/main/java/com/github/galatynf/forglory/imixin/IPlayerIDMixin.java
+++
@@ -1,6 +0,0 @@
-package com.github.galatynf.forglory.imixin;
-
-public interface IPlayerIDMixin {
-    Integer getPlayerID();
-    void setPlayerID(Integer playerID);
-}
