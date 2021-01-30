e32b6fd48658ea9ad71a30ec32b5895cdf96f69f
GalatynF <galvana.ciaran@outlook.fr>
2021-01-30 14:14:45 +0100

    Added resistance when counterattacking with shield feat
    Don't die while trying a cool move

--- src/main/java/com/github/galatynf/forglory/mixin/heal/ShieldPlayerMixin.java
+++ src/main/java/com/github/galatynf/forglory/mixin/heal/ShieldPlayerMixin.java
@@ -5,8 +5,12 @@ import com.github.galatynf.forglory.config.ModConfig;
 import com.github.galatynf.forglory.enumFeat.Feats;
 import com.github.galatynf.forglory.imixin.IShieldMixin;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -15,9 +19,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
-public abstract class ShieldPlayerMixin implements IShieldMixin {
+public abstract class ShieldPlayerMixin extends LivingEntity implements IShieldMixin {
     @Unique
     int forglory_lastBlocked = 0;
+
+    protected ShieldPlayerMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
     @Shadow public abstract void attack(Entity target);
 
@@ -42,6 +50,7 @@ public abstract class ShieldPlayerMixin implements IShieldMixin {
     public void counterattack(LivingEntity attacker, CallbackInfo ci) {
         if (Utils.canUseFeat(this, Feats.SUPER_SHIELD)) {
             if (forglory_lastBlocked != 0) {
+                this.applyStatusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 100, 1));
                 this.attack(attacker);
             }
         }
