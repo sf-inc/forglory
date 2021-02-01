15749510b429e9d9cf7878ae17f00c2dff497b5c
GalatynF <galvana.ciaran@outlook.fr>
2021-02-01 17:23:50 +0100

    Fixed shield resistance

--- src/main/java/com/github/galatynf/forglory/mixin/heal/ShieldResistanceMixin.java
+++ src/main/java/com/github/galatynf/forglory/mixin/heal/ShieldResistanceMixin.java
@@ -6,8 +6,11 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,13 +18,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LivingEntity.class)
 public abstract class ShieldResistanceMixin extends Entity {
 
+    @Shadow public abstract boolean isBlocking();
+
+    @Shadow protected ItemStack activeItemStack;
+
     public ShieldResistanceMixin(EntityType<?> type, World world) {
         super(type, world);
     }
 
     @Inject(at = @At("INVOKE"), method = "blockedByShield", cancellable = true)
     private void blockEverywhere(DamageSource source, CallbackInfoReturnable<Boolean> cir) {
-        if (Utils.canUseFeat(this, Feats.SHIELD_RESISTANCE)) {
+        if (Utils.canUseFeat(this, Feats.SHIELD_RESISTANCE)
+                && this.isBlocking()
+                && !source.isOutOfWorld()) {
+            if (this.activeItemStack.getItem() == Items.SHIELD) {
+                this.activeItemStack.setDamage(this.activeItemStack.getDamage() - 10);
+            }
             cir.setReturnValue(true);
         }
     }
