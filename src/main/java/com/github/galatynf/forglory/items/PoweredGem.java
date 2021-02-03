d511ceef1bd76608cdf113a9fbcddf0778ff8a55
Charly <charly5bollinger@gmail.com>
2021-02-03 22:29:27 +0100

    Powered gem loot when used
    They loot their associated gem (powerless) and randomly one essence item

--- src/main/java/com/github/galatynf/forglory/items/PoweredGem.java
+++ src/main/java/com/github/galatynf/forglory/items/PoweredGem.java
@@ -5,10 +5,14 @@ import com.github.galatynf.forglory.cardinal.MyComponents;
 import com.github.galatynf.forglory.config.ConstantsConfig;
 import com.github.galatynf.forglory.enumFeat.Feats;
 import com.github.galatynf.forglory.init.BlocksInit;
+import com.github.galatynf.forglory.init.ItemsInit;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -40,7 +44,23 @@ public abstract class PoweredGem extends Item {
                     world.setBlockState(pos, ((EssenceInfuser) BlocksInit.essenceInfuser).getState(false, false));
                 }
                 MyComponents.FEATS.get(user).addOrUpdateFeat(feat);
-                return new TypedActionResult<>(ActionResult.SUCCESS, user.getStackInHand(hand));
+                world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_GLASS_BREAK, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                user.getStackInHand(hand).decrement(1);
+
+                ItemEntity itemEntity;
+                ItemStack loot = new ItemStack(ItemsInit.essence);
+                loot.setCount(world.random.nextInt(2));
+                if (!loot.isEmpty()) {
+                    itemEntity = new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), loot);
+                    itemEntity.setToDefaultPickupDelay();
+                    world.spawnEntity(itemEntity);
+                }
+                loot = new ItemStack(this.gem);
+                itemEntity = new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), loot);
+                itemEntity.setToDefaultPickupDelay();
+                world.spawnEntity(itemEntity);
+
+                return new TypedActionResult<>(ActionResult.CONSUME, user.getStackInHand(hand));
             }
             return new TypedActionResult<>(ActionResult.FAIL, user.getStackInHand(hand));
         }
