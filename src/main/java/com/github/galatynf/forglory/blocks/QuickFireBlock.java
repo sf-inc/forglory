53c1e0a76e3e7c68ee21102c08058f24a7d83a12
Charly <charly5bollinger@gmail.com>
2021-02-03 21:06:35 +0100

    Quick fire doesn't destroy items

--- src/main/java/com/github/galatynf/forglory/blocks/QuickFireBlock.java
+++ src/main/java/com/github/galatynf/forglory/blocks/QuickFireBlock.java
@@ -6,6 +6,8 @@ import net.minecraft.block.AbstractFireBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
@@ -22,9 +24,10 @@ import java.util.Random;
 public class QuickFireBlock extends AbstractFireBlock {
     private static final IntProperty AGE = Properties.AGE_25;
     public static final BooleanProperty SHORT = BooleanProperty.of("short");
+    private static final float forglory_damage = 1.5F;
 
     public QuickFireBlock(Settings settings) {
-        super(settings, 1.5F);
+        super(settings, forglory_damage);
         setDefaultState(getStateManager().getDefaultState().with(SHORT, false));
     }
 
@@ -61,10 +64,17 @@ public class QuickFireBlock extends AbstractFireBlock {
             if ((feat1 != null && feat1.equals(Feats.FIRE_TRAIL)) ||
                     (feat2 != null && feat2.equals(Feats.FIRE_ZONE))) {
                 world.removeBlock(pos, false);
-                return;
+            }
+        } else if (entity instanceof LivingEntity) {
+            if (!entity.isFireImmune()) {
+                entity.setFireTicks(entity.getFireTicks() + 1);
+                if (entity.getFireTicks() == 0) {
+                    entity.setOnFireFor(8);
+                }
+
+                entity.damage(DamageSource.IN_FIRE, forglory_damage);
             }
         }
-        super.onEntityCollision(state, world, pos, entity);
     }
 
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
