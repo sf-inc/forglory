1c34c65bf3f346437d256b1747a8c033604c2c56
charlyb01 <charly5bollinger@gmail.com>
2024-08-29 17:55:46 +0200

    No hunger mixin refactor
    Do not call add exhaustion if lesser or equal to 7, set food level to 7 if less than 7

--- src/main/java/com/github/galatynf/forglory/mixin/mobility/NoHungerMixin.java
+++ src/main/java/com/github/galatynf/forglory/mixin/mobility/NoHungerMixin.java
@@ -2,32 +2,25 @@ package com.github.galatynf.forglory.mixin.mobility;
 
 import com.github.galatynf.forglory.Utils;
 import com.github.galatynf.forglory.enumFeat.Feats;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
-public abstract class NoHungerMixin extends LivingEntity {
+public class NoHungerMixin {
     @Shadow protected HungerManager hungerManager;
 
-    protected NoHungerMixin(EntityType<? extends LivingEntity> entityType, World world) {
-        super(entityType, world);
-    }
-
-    @Inject(method = "addExhaustion", at = @At("HEAD"), cancellable = true)
-    void preventHungerFromDropping(CallbackInfo ci) {
-        if (Utils.canUseFeat(this, Feats.NO_HUNGER)) {
-            if (this.hungerManager.getFoodLevel() < 7) {
-                this.hungerManager.setFoodLevel(7);
-                ci.cancel();
-            }
+    @WrapOperation(method = "addExhaustion", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/HungerManager;addExhaustion(F)V"))
+    private void preventHungerFromDropping(HungerManager instance, float exhaustion, Operation<Void> original) {
+        if (Utils.canUseFeat(this, Feats.NO_HUNGER)
+                && this.hungerManager.getFoodLevel() < 7) {
+            this.hungerManager.setFoodLevel(7);
+        } else if (this.hungerManager.getFoodLevel() > 7){
+            original.call(instance, exhaustion);
         }
     }
 }
